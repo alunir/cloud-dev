c35cb36c27795d3cdec1542239f0826069d2f49e
jimako <nsplat@gmail.com>
2020-07-10 14:50:50 +0900

    modified Dockerfiler to mount the storage

--- Dockerfile
+++ Dockerfile
@@ -27,5 +27,24 @@ RUN gcloud config configurations create cloud-dev; \
 
 RUN mkdir -p /root/.kube
 
+# mount
+RUN UUID=(blkid /dev/sdb | awk '{print $2}' | sed -e s/UUID=//g | sed -e 's/\"//g') && \
+    mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb && \
+    mkdir -p /mnt/disks && \
+    mount -o discard,defaults /dev/sdb /mnt/disks && \
+    chmod a+w /mnt/disks && \
+    cp /etc/fstab /etc/fstab.bkp && \
+    eval UUID="\$UUID" /mnt/disks ext4 discard,defaults,nobootwait 0 2 >> /etc/fstab && \
+    ln -s /mnt/disks/snap
+
+# install go with snap
+RUN mkdir -p /mnt/disks/go && \
+    snap install go && \
+    go env -w GOPATH=/mnt/disks/go && \
+    go env -w GOPROXY=direct && \
+    go env -w GOPRIVATE=github.com/alunir
+
+RUN snap install --classic code kubectl kompose
+
 COPY install.sh /root/
 
--- install.sh
+++ install.sh
@@ -23,32 +23,7 @@ gcloud auth activate-service-account 654650874191-compute@developer.gserviceacco
 pause
 gcloud auth configure-docker
 pause
-
-# mount
-echo "3. mount"
-read -p "Did you mount /dev/sdb?"
-UUID=(blkid /dev/sdb | awk '{print $2}' | sed -e s/UUID=//g | sed -e 's/\"//g')
-mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-mkdir -p /mnt/disks
-mount -o discard,defaults /dev/sdb /mnt/disks
-chmod a+w /mnt/disks
-cp /etc/fstab /etc/fstab.bkp
-eval UUID="\$UUID" /mnt/disks ext4 discard,defaults,nobootwait 0 2 >> /etc/fstab
-ln -s /mnt/disks/snap
-
-# snap
-echo "4. snap"
-read -p "snap install go?"
-snap install go; \
-go env -w GOPATH=/mnt/disks/go; \
-go env -w GOPROXY=direct; \
-go env -w GOPRIVATE=github.com/alunir
-
-read -o "snap install VSCode, kubectl, kompose?"
-snap install --classic code kubectl kompose
-
 read -p "gcloud container clusters get-credentials"
 gcloud container clusters get-credentials alunir --region us-central1-a
 
 echo "Finished!"
-
