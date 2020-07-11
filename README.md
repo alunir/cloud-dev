b199d05db97a0379eebaaecacbb1737afb109143
jimako <nsplat@gmail.com>
2020-07-11 15:22:58 +0900

    Modified cloud-dev.sh

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cloud-dev
 GCE image for development via RDP
+
+## How to use
+ 1. Create an instance from instance template 'cloud-dev'
+ :warning: NOTE: DO NOT FORGET SETTING THE NEAREST REGION FROM YOUR LOCATION
+ 2. Connect the instance via SSH
+ 3. Save ~/.ssh/id_rsa
+ 4. Create a directory ~/.kube
+ 5. Save ~/.kube/service_account_gcp.json
+ 6. Launch this script
+```
+cd ~/
+git clone http://github.com/alunir/cloud-dev
+chmod +x ./cloud-dev/cloud-dev.sh
+./cloud-dev/cloud-dev.sh
+```
--- cloud-dev.sh
+++ cloud-dev.sh
@@ -1,92 +1,113 @@
 #!/bin/sh
-echo "Usage: ./cloud-dev.sh INSTANCE_NAME"
-
-INSTANCE_NAME=$1
-USER_NAME=jimako
+echo "Interactive install commands for cloud-dev"
 
 if [ ! -e ~/.ssh/id_rsa ]; then
   echo "Not found ~/.ssh/id_rsa"
   exit 1
 fi
-chmod 644 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
 if [ ! -e ~/.kube/service_account_gcp.json ]; then
   echo "Not found ~/.kube/service_account_gcp.json"
   exit 1
 fi
 
+echo "Set GIT_USER_NAME (e.g. jimako)"
+read GIT_USER_NAME
+
+echo "Set GIT_USER_EMAIL (e.g. nsplat@gmail.com)"
+read GIT_USER_EMAIL
+
+echo "Set GOOGLE_ACCOUNT_ID (e.g. nsplat@gmail.com)"
+read GOOGLE_ACCOUNT_ID
+
+echo "Set VM_REGION (e.g. asia-northeast1)"
+read VM_REGION
+
 # User Setting
-sudo adduser $USER_NAME
-sudo gpasswd -a $USER_NAME sudo
-sudo passwd $USER_NAME
+sudo adduser $USER
+sudo gpasswd -a $USER sudo
+sudo passwd $USER
+
+# mount
+# see. https://cloud.google.com/compute/docs/disks/add-persistent-disk?hl=ja#formatting
+# sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+# sudo mkdir -p /mnt/disks
+# sudo mount -o discard,defaults /dev/sdb /mnt/disks
+# sudo chmod a+w /mnt/disks
+# sudo cp /etc/fstab /etc/fstab.bkp
+# UUID=`sudo blkid /dev/sdb | awk '{print $2}' | sed -e s/UUID=//g | sed -e 's/\"//g'`
+# echo "UUID: "$UUID
+# echo UUID="${UUID}" /mnt/disks ext4 discard,defaults,nobootwait 0 2 | sudo tee -a /etc/fstab
+# sudo mkdir -p /mnt/disks/snap
+# sudo ln -s /mnt/disks/snap /
 
 # Skip interactive installation for git
 export DEBIAN_FRONTEND=noninteractive
 
-apt update && apt install -y sudo gnome-core xrdp
+sudo apt update && sudo apt install -y gnome-core xrdp
 
-apt-get install -y docker.io
+sudo apt-get install -y docker.io
 docker --version
 
 ## git configuration
 # http://crossbridge-lab.hatenablog.com/entry/2016/11/11/073000
-apt-get install -y openssh-client
-mkdir -p /root/.ssh
-echo 'Host github.com\n  HostName github.com\n  IdentityFile ~/.ssh/id_rsa\n  User git' > /root/.ssh/config
+sudo apt-get install -y openssh-client
+mkdir -p ~/.ssh
+sudo echo 'Host github.com\n  HostName github.com\n  IdentityFile ~/.ssh/id_rsa\n  User git' > ~//.ssh/config
 
-git config --global user.email "nsplat@gmail.com"
-git config --global user.name "jimako"
-
-ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
+git config --global user.email $GIT_USER_EMAIL
+git config --global user.name $GIT_USER_NAME
 
 # gcloud configuration
 # https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu?hl=ja
-apt-get install -y curl
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+sudo apt-get install -y curl
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 
 gcloud config configurations create cloud-dev
-gcloud config set account nsplat@gmail.com
+gcloud config set account GOOGLE_ACCOUNT_ID
 
-mkdir -p /root/.kube
+read -p "Starting gcloud auth login"
+gcloud auth login
 
+# see https://cloud.google.com/compute/docs/ip-addresses/reserve-static-external-ip-address?hl=ja#promote_ephemeral_ip
+IP_ADDRESS=`curl ifconfig.io`
+echo $IP_ADDRESS
 
+gcloud compute addresses create static-ip --addresses $IP_ADDRESS --region $VM_REGION
+
+ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 # git
-ssh -T git@github.com & echo 'SSH Connection Success!'
+ssh -T git@github.com || echo 'SSH Connection Success!'
 
-cd ~/Document
+
+cd ~/
 # https://qiita.com/rorensu2236/items/df7d4c2cf621eeddd468
 git clone ssh://git@github.com/alunir/alunir
 cd ./alunir; git remote set-url origin git@github.com:alunir/alunir.git
 
-read -p "Starting gcloud auth login"
-gcloud auth login
-gcloud auth activate-service-account 654650874191-compute@developer.gserviceaccount.com --key-file=/root/.kube/service_account_gcp.json
+
+# ???
+# gcloud auth activate-service-account 654650874191-compute@developer.gserviceaccount.com --key-file=~/.kube/service_account_gcp.json
 gcloud auth configure-docker
 
-read -p "Set this IP to the static IP..."
-IP_ADDRESS=`ip -4 a show ens4 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
-gcloud compute addresses create "cloud-dev" --addresses IP_ADDRESS --region "asia-northeast1-b" --global
-
-# mount
-UUID=`blkid /dev/sdb | awk '{print $2}' | sed -e s/UUID=//g | sed -e 's/\"//g'`
-mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-mkdir -p /mnt/disks
-mount -o discard,defaults /dev/sdb /mnt/disks
-chmod a+w /mnt/disks
-cp /etc/fstab /etc/fstab.bkp
-UUID="${UUID}" /mnt/disks ext4 discard,defaults,nobootwait 0 2 >> /etc/fstab
-ln -s /mnt/disks/snap
-
 # install go with snap
-mkdir -p /mnt/disks/go
-snap install go
-go env -w GOPATH=/mnt/disks/go
+mkdir -p /home/$USER/go
+sudo snap install --classic go
+go env -w GOPATH=/home/$USER/go
 go env -w GOPROXY=direct
 go env -w GOPRIVATE=github.com/alunir
 
-RUN snap install --classic code kubectl kompose
+sudo snap install --classic code
+sudo snap install --classic kubectl
+sudo snap install --classic kompose
 
 read -p "gcloud container clusters get-credentials"
 gcloud container clusters get-credentials alunir --region us-central1-a
 
-echo "Finished!"
+echo "Finished! Recommend to save here to a VM Image"
